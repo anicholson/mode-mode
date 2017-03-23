a595d5eee7da81af78887de83988c5a1ed451b79
Andy Nicholson <andrew@anicholson.net>
2017-03-23 15:46:11 +1100

    Front view skeleton taking shape

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,24 +1,59 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (Html, p, h1, h2, div, section, input, text, program)
+import Html.Attributes exposing (class, placeholder)
 
-type alias Model = String
 
-init : (Model, Cmd Msg)
-init = ( "Hello", Cmd.none )
+type alias Mode =
+    { name : String
+    , url : String
+    }
 
-type Msg = NoOp
+
+type alias Model =
+    { searchListing : Maybe String
+    , currentModes : List Mode
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { searchListing = Nothing, currentModes = [] }, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+renderMode : Mode -> Html Msg
+renderMode mode =
+    p [ class "mode" ] [ text mode.name ]
+
 
 view : Model -> Html Msg
 view model =
     div []
-        [ text model ]
+        [ section [ class "section hero is-primary" ]
+            [ h1 [ class "title" ] [ text "mode-mode" ]
+            , h2 [ class "subtitle" ] [ text "(emacs-mode-for-that-p 'x) => t" ]
+            ]
+        , section [ class "section" ]
+            [ input [ class "input", placeholder "Search for a mode" ] []
+            ]
+        , section [ class "section" ]
+            (List.map
+                renderMode
+                model.currentModes
+            )
+        ]
 
-update : Msg -> Model -> ( Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
